2f703429fe5970cd97613b24e34f9f9e349bbb3f
vijaydurai <vijaydurai.me@gmail.com>
2025-10-28 12:47:09 +0530

    Fixed: port configuration

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -56,3 +56,9 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+
+#Attach NSG to NIC
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
